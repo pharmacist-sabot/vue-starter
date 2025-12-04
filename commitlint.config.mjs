d654c059e90f06ccb4b33eda67c78635ca3c86e6
Suradet P150 <pharmacistsabot@gmail.com>
2025-12-04 13:15:09 +0700

    chore: update configuration settings

---
+++ commitlint.config.mjs
@@ -0,0 +1,24 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ],
+    'subject-full-stop': [2, 'never', '.'],
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './assets/main.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import './assets/main.css';
+import App from './App.vue';
+
+createApp(App).mount('#app');
